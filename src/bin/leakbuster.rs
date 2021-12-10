61ed83f550a5f8e35b8e99e14b8f41c709bfbaa5
Olaf Czarnecki <olaf.czarnecki@outlook.com>
2021-12-11 00:03:30 +0100

    Implemented arg parsing

--- src/bin/leakbuster.rs
+++ src/bin/leakbuster.rs
@@ -1,5 +1,30 @@
 use leakbuster::cmd::run;
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+/// Leakbuster is a tool for monitoring the time for which applications are
+/// running, and for triggering configurable events once the usage time
+/// meets some condition.
+enum Leakbuster {
+    /// Run an app, defined in config, and trigger startup and time hooks
+    Run {
+        #[structopt(parse(from_os_str))]
+        /// Path to the configuration file
+        config: PathBuf,
+
+        #[structopt(parse(from_os_str))]
+        /// Path to the usage db
+        db: PathBuf,
+
+        /// Id of the app to start, as defined in config
+        app_id: String
+    }
+}
 
 fn main() {
-    run::run("test-config.yaml", "usage.sqlite", "testapp");
+    let leakbuster = Leakbuster::from_args();
+    match leakbuster {
+        Leakbuster::Run{config, db, app_id} => run::run(config, db, &app_id)
+    }
 }
